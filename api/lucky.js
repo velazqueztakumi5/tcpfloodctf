60fee5dd188ea6d90635b3b15b00ebd35109882b
velazqueztakumi5 <matematicases555@gmail.com>
2026-04-15 21:04:49 -0600

    Upload the thing

---
+++ api/lucky.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  const FLAG = process.env.CTF_FLAG;
+  const roll = Math.floor(Math.random() * 100000) + 1;
+  const result = roll === 1 ? FLAG : randomString(32);
+  res.setHeader("Content-Type", "text/plain");
+  res.send(result);
+}
+
+function randomString(len) {
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  return Array.from({ length: len }, () =>
+    chars[Math.floor(Math.random() * chars.length)]
+  ).join("");
+}
